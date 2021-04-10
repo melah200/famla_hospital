68f86d19e6d690ffd7cb9037bbc4cac3fed3bbfa
melah200 <45556366+melah200@users.noreply.github.com>
2021-04-10 20:04:57 +0200

    update

--- hms/dashboard/inc/connect.php
+++ hms/dashboard/inc/connect.php
@@ -1,6 +1,11 @@
 <?php
 //error_reporting(0);
+if(!isset($_SESSION["username"]))
+{
+	header("Location: ../../");
+}
 $db_connect =mysqli_connect("localhost","root","");
+$connection = $db_connect;
 mysqli_select_db($db_connect, "hms") or die(mysql_error($db_connect));
 
 ?>
