71068639b11dc755345d55e66dcc61094fa12a2f
melah200 <45556366+melah200@users.noreply.github.com>
2021-05-08 16:30:07 +0200

    period changed to start and end date

--- hms/dashboard/Patient/casehistory.php
+++ hms/dashboard/Patient/casehistory.php
@@ -72,7 +72,7 @@ function mysql_fetch_all($query) {
                   <label >Start Date</label>
                   <input type="date" name="date" class="form-control" id="exampleInputPassword1" placeholder="" value="<?php echo date('Y-m-d');  ?>"><br>
                   <label >End Date</label>
-				  <input type="date" name="date" class="form-control" placeholder="" value="<?php echo date('Y-m-d');  ?>"><br>
+				  <input type="date" name="enddate" class="form-control" placeholder="" value="<?php echo date('Y-m-d');  ?>"><br>
                    <label >Patient</label><br>
                   <select name="patient" class="form-control select2"  placeholder="">
                   <?php foreach ($p_row1 as $p) {?>
