fbeb64261c343a8519e3a21b6c49b796d11ae425
melah200 <45556366+melah200@users.noreply.github.com>
2021-06-13 11:22:13 +0200

    edit is removed edit_activity is added

--- hms/dashboard/Patientrecords/edit_activity.php
+++ hms/dashboard/Patientrecords/edit_activity.php
@@ -17,9 +17,9 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
   $date = escape($_POST['date']);
   $activityId = escape($_GET['entry']);
   
-  $activitie = escape($_POST['activitie']);
+  $activity = escape($_POST['activity']);
   
-  $queryUpdate = "UPDATE activity set dateMassnahme='$date', massnahme='$activitie' WHERE idA='$activityId' AND pid='$patientId'";
+  $queryUpdate = "UPDATE activity set dateMassnahme='$date', massnahme='$activity' WHERE idA='$activityId' AND pid='$patientId'";
   
   $queryAddResult=mysqli_query($db_connect, $queryUpdate)or die (mysqli_error($db_connect));
   header("Location: patientrecordoverview.php?id=$patientId");
@@ -36,13 +36,13 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 <div class="content-wrapper">
     <section class="content-header">
       <h1>
-        Patient Add new Activitie
+        Add new patient activity
         <small></small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Patient record</li>
-        <li class="active">Activitie</li>
+        <li class="active">Activities</li>
       </ol>
     </section>
 
@@ -51,75 +51,9 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 	<div class="col-xs-12">
     <div class="box box-primary">
       <div class="box-header with-border ">
-        <i class="fa fa-user"></i> <h3 class="box-title">Activitie</h3>
+        <i class="fa fa-user"></i> <h3 class="box-title">Activities</h3>
       </div>
 	  
-      <div class="modal fade" id="myModal" role="dialog">
-       <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Patient Register</h4>
-          </div>
-          <div class="modal-body">
-            <form method="POST" enctype="multipart/form-data">
-             <!--    <div class="form-group">
-                 <label for="exampleInputEmail1">Doctor</label>
-                 <input type="name" class="form-control" name="doctor" placeholder="">
-                </div> -->
-                <div class="form-group">
-                <label for="exampleInputEmail1">Full Name</label>
-                <input type="text" name="name" class="form-control" placeholder="" required="">
-               </div>
-               <div class="form-group">
-               <label for="exampleInputEmail1">Email</label>
-               <input type="Email" name="email" class="form-control" placeholder="" required="">
-              </div>
-             <!--   <div class="form-group">
-              <label for="exampleInputFile">Password</label>
-              <input type="Password" name="password" class="form-control" placeholder="">
-              </div> -->
-               <div class="form-group">
-               <label for="exampleInputPassword1">Address</label>
-              <input type="text" name="address"class="form-control" placeholder="" required="">
-               </div>
-               <div class="form-group">
-              <label for="exampleInputPassword1">Phone</label>
-              <input type="text" name="phone" class="form-control" placeholder="" required="">
-              </div>
-              <div class="form-group">
-              <label for="exampleInputPassword1">Gender</label>
-              <select name="gender" class="form-control" placeholder="" required="">
-              <option value="" disabled selected="selected"> Select Category</option>
-              <option value="Male">Male</option> <option value="Female">Female</option>
-              <option value="Other">Other</option>
-              </select>
-            </div>
-             <div class="form-group">
-            <label for="exampleInputPassword1">Birthdate</label>
-            <input type="date" name="birthdate" class="form-control" placeholder="" required="">
-           </div>
-           <div class="form-group">
-            <label for="exampleInputPassword1">Bloodgroup</label>
-          <select name="bloodgroup" class="form-control" id="c" placeholder="" required="">
-            <option value="" disabled selected="selected"> Select Category</option>
-            <option value="A+">A+</option> <option value="A-">A-</option>
-            <option value="B+">B+</option><option value="B-">B-</option> <option value="AB+">AB+</option> <option value="AB-">AB-</option> <option value="O+">O+</option><option value="O-">O-</option>
-          </select>
-          </div>
-          <td><b>Image Upload</b></font>
-          <input type="file" name="imageupload" id="fileToUpload" required=""></td>
-           <div class="box-footer">
-          <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-         </div>
-         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </form>
-      </div>
-    </div>
-      </div>
-      </div>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <?php 
@@ -134,6 +68,9 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
     <p>Gender	: &nbsp;&nbsp;&nbsp;<?php echo $patient['gender']; ?></p>
 
 </div>
+<div class="box-body">
+	<div class="box box-success box-body">
+	  <div class="modal-body">
 <?php } ?>
 	  <?php if(isset($_GET['recordtyp']) && $_GET['recordtyp']=="activity" && !isset($_GET['entry'])){ ?>
 	   <!-- Section Activities to edit -->
@@ -162,7 +99,7 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 						  <!--<th scope="row">1</th>-->
 						  <td><?php echo $row['dateMassnahme']; ?></td>
 						  <td><?php echo $row['massnahme']; ?></td>	
-						  <td class="rows"><a name="edit" href="edit.php?id=<?php echo $patientId; ?>&recordtyp=activity&entry=<?php echo $row['idA']; ?>"><span class="btn btn-success"><i class="fa fa-edit"></i> Edit </span></a></td>
+						  <td class="rows"><a name="edit" href="edit_activity.php?id=<?php echo $patientId; ?>&recordtyp=activity&entry=<?php echo $row['idA']; ?>"><span class="btn btn-success"><i class="fa fa-edit"></i> Edit </span></a></td>
 						<!--  <td class="rows"><input type="checkbox" class="form-check-input check" name="entry2delete[]"/></td>
 						-->
 						</tr>
@@ -195,9 +132,7 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 		}
 	  
 	  ?>	
-<div class="box-body">
-	<div class="box box-success box-body">
-	  <div class="modal-body">
+
 			<form method="POST" enctype="multipart/form-data">
 
 			   <div class="form-group">
@@ -205,8 +140,8 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 				  <input type="date" name="date" class="form-control" id="date" placeholder="" value="<?php if($editAct != "") echo $editAct['dateMassnahme']; ?>" required>
 			   </div>
 			   <div class="form-group">
-				   <label for="typ">Activitie</label>
-				  <input type="text" name="activitie" class="form-control" id="activitie" placeholder="" value="<?php if($editAct != "") echo $editAct['massnahme']; ?>" required>
+				   <label for="typ">Activity</label>
+				  <input type="text" name="activity" class="form-control" id="activity" placeholder="" value="<?php if($editAct != "") echo $editAct['massnahme']; ?>" required>
 			   </div>
 			   
 
@@ -218,11 +153,11 @@ if(isset($_POST['submit']) && isset($_GET['entry'])){
 				<a type="button" name="close" class="btn btn-default" href="patientrecordoverview.php?id=<?php echo $patientId; ?>">Close</a>
 			  </div>
 			  </form>
+      <?php } ?>
 		  </div>
 </div>
         
         </div>
-      <?php } ?>
 	  </div>       
       </div>
     </section>
